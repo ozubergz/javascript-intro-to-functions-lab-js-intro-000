05d52952d48eb6f173d1ce315fffab23ea6961d1
James Min <jmin499@gmail.com>
2020-09-25 17:19:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(whisper(string) === string) {
-    return "I can't hear you"
+    return "I can't hear you!"
+  } else if(shout(string) === string) {
+    return ""
   }
 }
